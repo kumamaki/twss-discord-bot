3aa7522f866f2e518aec595a886ec1b7aeb724bd
kumamaki <8641684+kumamaki@users.noreply.github.com>
2021-09-18 17:11:41 +0430

    :tada: Initial commit

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const client = new Discord.Client({
+    intents: [
+        Discord.Intents.FLAGS.GUILDS
+    ]
+});
 
 client.on('message', msg => {
   if (msg.content === 'ping') {
