43cf8a4b6e18d185f075998a077eb9719b3f0bcc
kumamaki <8641684+kumamaki@users.noreply.github.com>
2021-09-18 17:45:42 +0430

    :tada: Initial commit

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 require('dotenv').config();
-const express = require('express');
+
 const Discord = require('discord.js');
+const twss = require('twss');
 const client = new Discord.Client({
     intents: [
-        Discord.Intents.FLAGS.GUILDS
+        Discord.Intents.FLAGS.GUILDS,
+        Discord.Intents.FLAGS.GUILD_MESSAGES
     ]
 });
 
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}`);
+})
+
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
+    twss.threshold = 0.75;
+    console.log('msg.content 👉 ', msg.content);
+    console.log('twss.prob(msg.content) 👉 ', twss.prob(msg.content));
+    if (
+        twss.is(msg.content) &&
+        msg.content !== `That's what she said`
+    ) {
+        msg.reply(`That's what she said`);
+    }
 });
 
 client.login(process.env.CLIENT_TOKEN);
